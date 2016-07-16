733f2d1d43c57c961e56f1f270f347755e426e03
Sean Connor <seantconnor@gmail.com>
2016-07-15 21:44:22 -0700

    able to get tm data with node

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
+var url = 'http://concerts.livenation.com/microsite/settlement'
+
 var options = {
   method: 'get',
-  url: 'http://concerts.livenation.com/microsite/settlement',
+  url: url,
   withCredentials: false
 }
 
-axios(options)
-.then(function(response) {
-  console.log(response);
-})
+// axios(options)
+// .then(function(response) {
+//   console.log(response);
+// })
+// $.get(url, function( my_var ) {
+//   console.log('mv',my_var)
+//     // my_var contains whatever that request returned
+// });
+$('.container').load(url); // SERIOUSLY!
+
+// $.ajax({
+//     url: url,
+//     type: 'GET',
+//     success: function(res) {
+//       console.log('ress',res);
+//         // var headline = $(res.responseText).find('a.tsh').text();
+//         // alert(headline);
+//     }
+// });
